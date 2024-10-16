015eb8840306bf57287e4648d3ff334fa5a2be58
lukasordia123 <lksordia@gmail.com>
2024-10-16 18:21:23 +0400

    Update Emailcs.cs

--- Data/Emailcs.cs
+++ Data/Emailcs.cs
@@ -10,7 +10,7 @@ namespace MyUni.Data
         private const string SmtpServer = "smtp-relay.brevo.com";
         private const int SmtpPort = 587;
         private const string SmtpUser = "7df9dc001@smtp-brevo.com";
-        private const string SmtpPass = "xMyRkZOdmP7s6VKa"; // Your SMTP Master Password
+        private const string SmtpPass = "xMyRkZOdmP7s6VKa"; 
         public Emailcs(ApplicationDbContext dbContext)
         {
             this.dbContext = dbContext;
@@ -56,65 +56,3 @@ namespace MyUni.Data
     }
 }
 
-
-
-// using System.Net;
-// using System.Net.Mail;
-// using System.Threading.Tasks;
-
-// namespace MyUni.Data
-// {
-//     public class Emailcs
-//     {
-//         private readonly ApplicationDbContext dbContext;
-//         private const string SmtpServer = "smtp-relay.brevo.com";
-//         private const int SmtpPort = 587;
-//         private readonly string SmtpUser;
-//         private readonly string SmtpPass;
-
-//         public Emailcs(ApplicationDbContext dbContext)
-//         {
-//             this.dbContext = dbContext;
-//             SmtpUser = Environment.GetEnvironmentVariable("7df9dc001@smtp-brevo.com"); // Store this in environment variables
-//             SmtpPass = Environment.GetEnvironmentVariable("xsmtpsib-6eb4b545578f7c5a27aaf563a92cfd781fbfbab62ada56f13bee07fc10c33726-NOGd32qLRaCnPhxp"); // Store this in environment variables
-//         }
-
-//         public async Task SendEmailToAllUsers(string subject, string body)
-//         {
-//             var users = dbContext.MyUser.ToList(); // Assuming you have a Users DbSet
-
-//             foreach (var user in users)
-//             {
-//                 await SendEmail(user.Email, subject, body);
-//             }
-//         }
-
-//         private async Task SendEmail(string email, string subject, string body)
-//         {
-//             try
-//             {
-//                 using (var smtpClient = new SmtpClient(SmtpServer, SmtpPort))
-//                 {
-//                     smtpClient.Credentials = new NetworkCredential(SmtpUser, SmtpPass);
-//                     smtpClient.EnableSsl = true;
-
-//                     var mailMessage = new MailMessage
-//                     {
-//                         From = new MailAddress(SmtpUser), // Use the same email as SMTP User
-//                         Subject = subject,
-//                         Body = body,
-//                         IsBodyHtml = true
-//                     };
-
-//                     mailMessage.To.Add(email);
-
-//                     await smtpClient.SendMailAsync(mailMessage);
-//                 }
-//             }
-//             catch (Exception ex)
-//             {
-//                 Console.WriteLine($"Failed to send email to {email}: {ex.Message}");
-//             }
-//         }
-//     }
-// }
