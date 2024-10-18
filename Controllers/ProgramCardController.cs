bfe5d6be6d6d8680b7b6e95ba1981bdc7b721312
gogiadata01 <gogia.data11@gmail.com>
2024-10-18 15:20:40 +0400

    Update ProgramCardController.cs

--- Controllers/ProgramCardController.cs
+++ Controllers/ProgramCardController.cs
@@ -33,7 +33,7 @@ public async Task<IActionResult> GetAllProgramCards()
             field.FieldName, // Only include FieldName
             ProgramNames = field.ProgramNames.Select(program => new
             {
-                program.id
+                program.id,
                 program.programname // Only include programname
             }).ToList()
         }).ToList()
