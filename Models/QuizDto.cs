d8cd7f06233671e4b741c53e67b01cbe93ca536c
lukasordia123 <lksordia@gmail.com>
2024-11-08 18:41:16 +0400

    Update QuizDto.cs

--- Models/QuizDto.cs
+++ Models/QuizDto.cs
@@ -1,35 +1,33 @@
-﻿namespace MyUni.Models.Entities
-{
-    public class Quiz
-    {
-        public int Id { get; set; }
-        public string Time { get; set; }
-        public ICollection<Question> Questions { get; set; }
-        public BonusQuestionDetails BonusQuestion { get; set; }
+﻿using static MyUni.Models.Entities.Quiz;
 
-        public class Question
+namespace MyUni.Models
+{
+    public class QuizDto
+    {
+ public string Time { get; set; }
+        public ICollection<Question> Questions { get; set; }
+        public BonusQuestionDetails BonusQuestion { get; set; } // Match this name with the controller
+
+         public class Question
         {
-            public int Id { get; set; }
             public string question { get; set; }
             public string correctanswer { get; set; }
-            public string img { get; set; }
+            public string img { get; set; } = null;
             public ICollection<IncorrectAnswer> IncorrectAnswers { get; set; }
         }
 
         public class IncorrectAnswer
         {
-            public int Id { get; set; }
-            public string IncorrectAnswer { get; set; } // Fixed the typo here
+            public string InccorectAnswer { get; set; } // Ensure spelling is consistent
         }
-
-        public class BonusQuestionDetails
+        public class BonusQuestionDetails // Use this class name consistently in both model and controller
         {
-            public int Id { get; set; }
             public string question { get; set; }
             public string correctanswer { get; set; }
             public string img { get; set; }
             public ICollection<IncorrectAnswer> IncorrectAnswers { get; set; }
-            public int Coins { get; set; } = 3;
+            public int Coins { get; set; } = 3; // Bonus question worth 3 coins
         }
     }
+
 }
