051b66f7873c34eb32f7079a51e93ee20d421077
lukasordia123 <lksordia@gmail.com>
2024-11-14 00:48:27 +0400

    Update UniversityVisit.cs

--- Models/Entities/UniversityVisit.cs
+++ Models/Entities/UniversityVisit.cs
@@ -4,7 +4,6 @@ namespace MyUni.Models.Entities
     {
         public int Id { get; set; }
         public string UniversityName { get; set; }
-        public string UserId { get; set; }
-        public DateTime VisitDate { get; set; }
+        public DateTime? VisitDate { get; set; } // Nullable DateTime
     }
 }
