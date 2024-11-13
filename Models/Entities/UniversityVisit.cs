070fb9f1071e6a279ac8ea090e156bba2964afd5
lukasordia123 <lksordia@gmail.com>
2024-11-13 23:04:09 +0400

    Update UniversityVisit.cs

--- Models/Entities/UniversityVisit.cs
+++ Models/Entities/UniversityVisit.cs
@@ -1,17 +1,17 @@
 using System;
 using System.ComponentModel.DataAnnotations;
 
-namespace MyUni.Models
+namespace MyUni.Models.Entities // Updated to match other entities
 {
     public class UniversityVisit
     {
         [Key]
         public int Id { get; set; }
-        
+
         public string UniversityName { get; set; }
-        
+
         public string UserId { get; set; }
-        
+
         public DateTime VisitDate { get; set; }
     }
 }
