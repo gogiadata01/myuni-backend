0bdb98b788f9acf158060f77d1ab1e2ce1760065
lukasordia123 <lksordia@gmail.com>
2024-11-14 11:03:18 +0400

    ..

---
+++ Models/UniversityVisitDto.cs
@@ -0,0 +1,8 @@
+namespace MyUni.Models
+{
+        public class UniversityVisitDto
+    {
+        public string UniversityName { get; set; }
+        public DateTime? VisitDate { get; set; } // Nullable DateTime
+    }
+}
