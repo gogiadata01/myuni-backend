f4a46467b0180c820e75edf7803d1b7d56947f8d
lukasordia123 <lksordia@gmail.com>
2024-11-16 02:48:52 +0400

    Update QuizDto.cs

--- Models/QuizDto.cs
+++ Models/QuizDto.cs
@@ -4,7 +4,7 @@
     {
         public string Time { get; set; }
         public ICollection<Question> Questions { get; set; }
-        public BonusQuestionDetails BonusQuestion { get; set; }
+        public BonusQuestionDetails? BonusQuestion { get; set; }
 
         public class Question
         {
@@ -21,15 +21,11 @@
 
         public class BonusQuestionDetails
         {
-            public string question { get; set; }
-            public ICollection<correctanswers> CorrectAnswers { get; set; }
-            public string img { get; set; }
-            public ICollection<IncorrectAnswer> IncorrectAnswers { get; set; }
+            public string? question { get; set; }
+            public string? correctanswer { get; set; }
+            public string? img { get; set; }
+            public ICollection<IncorrectAnswer>? IncorrectAnswers { get; set; }
             public int Coins { get; set; } = 3; // Bonus question worth 3 coins
-        }
-             
-        public class correctanswers {
-            public string correctanswer { get; set; }
         }
     }
 }
