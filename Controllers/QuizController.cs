ebb68d2597277db79c33182c622fc54ae9c4d6ee
lukasordia123 <lksordia@gmail.com>
2024-11-20 18:47:27 +0400

    Update QuizController.cs

--- Controllers/QuizController.cs
+++ Controllers/QuizController.cs
@@ -225,7 +225,7 @@ public IActionResult SendCustomEmail([FromBody] EmailRequestDto emailRequest)
         {
             // Fetch the email list
             var emailList = sendToAllUsers
-                ? dbContext.Users.Select(u => u.Email).ToList() // Fetch all emails from the database
+                ? dbContext.MyUser.Select(u => u.Email).ToList() // Fetch all emails from the database
                 : emailRequest.Emails;
 
             // Call the email service to send emails
