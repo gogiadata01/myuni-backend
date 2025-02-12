7b8f32bf9491308865c924bbef35469b88ead3c2
lukasordia123 <lksordia@gmail.com>
2025-02-13 02:16:13 +0400

    Create UniCardDetails.cs

---
+++ Models/UniCardDetails.cs
@@ -0,0 +1,16 @@
+namespace MyUni.Models
+{
+public class UniCardDetails
+{
+    public string? Url { get; set; }
+    public string? Title { get; set; }
+    public string? MainText { get; set; }
+    public string? History { get; set; }
+    public string? ForPupil { get; set; }
+    public string? ScholarshipAndFunding { get; set; }
+    public string? ExchangePrograms { get; set; }
+    public string? Labs { get; set; }
+    public string? StudentsLife { get; set; }
+    public string? PaymentMethods { get; set; }
+}
+}
