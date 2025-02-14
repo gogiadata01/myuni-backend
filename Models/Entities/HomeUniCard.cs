ff268f60e417a47a2740a501d135b0ef5e4b84f5
lukasordia123 <lksordia@gmail.com>
2025-02-15 03:28:40 +0400

    Update HomeUniCard.cs

--- Models/Entities/HomeUniCard.cs
+++ Models/Entities/HomeUniCard.cs
@@ -31,6 +31,7 @@ namespace MyUni.Models.Entities
             public string Url { get; set; }
             public string Title { get; set; }
             public string Text { get; set; }
+                public string TextLink { get; set; } // <- Ensure this exists before removal
         }
 
         public class Section
