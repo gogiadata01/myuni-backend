b6e9e10917cb15570a176f584babc93d2f601a1c
lukasordia123 <lksordia@gmail.com>
2025-02-15 03:44:11 +0400

    Update EventDto.cs

--- Models/EventDto.cs
+++ Models/EventDto.cs
@@ -6,7 +6,7 @@
         public string Title { get; set; }
         public string Text { get; set; }
         public string Time { get; set; } 
-         public string Link { get; set; }
+        public string Link { get; set; }
 
     }
 }
