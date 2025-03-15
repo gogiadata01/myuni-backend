40b1caba12f6852bb99acd653c9c62667e6fbbf5
lukasordia123 <lksordia@gmail.com>
2025-03-15 16:09:00 +0400

    as

--- Models/Entities/QuizArchive.cs
+++ Models/Entities/QuizArchive.cs
@@ -4,38 +4,46 @@ namespace MyUni.Models.Entities
     {
         public int Id { get; set; }
         public string Time { get; set; }
-        public ICollection<Question> Questions { get; set; }
-        public BonusQuestionDetails? BonusQuestion { get; set; }
+        public ICollection<ArchivedQuestion> Questions { get; set; }
+        public int? BonusQuestionId { get; set; }
+        public ArchivedBonusQuestionDetails? BonusQuestion { get; set; }
+    }
 
-        public class Question
-        {
-            public int Id { get; set; }
-            public string question { get; set; }
-            public string correctanswer { get; set; }
-            public string img { get; set; } = null;
-            public ICollection<IncorrectAnswer> IncorrectAnswers { get; set; }
-        }
+    public class ArchivedQuestion
+    {
+        public int Id { get; set; }
+        public string question { get; set; }
+        public string correctanswer { get; set; }
+        public string img { get; set; } = null;
+        public int QuizArchiveId { get; set; }
+        public QuizArchive QuizArchive { get; set; }
+        public ICollection<ArchivedIncorrectAnswer> IncorrectAnswers { get; set; }
+    }
 
-        public class IncorrectAnswer
-        {
-            public int Id { get; set; }
-            public string InccorectAnswer { get; set; }
-        }
+    public class ArchivedIncorrectAnswer
+    {
+        public int Id { get; set; }
+        public string InccorectAnswer { get; set; }
+        public int ArchivedQuestionId { get; set; }
+        public ArchivedQuestion ArchivedQuestion { get; set; }
+    }
 
-        public class BonusQuestionDetails
-        {
-            public int Id { get; set; }
-            public string? question { get; set; }
-            public string? img { get; set; }
-            public ICollection<correctanswers>? CorrectAnswers { get; set; }
-            public ICollection<IncorrectAnswer>? IncorrectAnswers { get; set; }
-            public int Coins { get; set; } = 3;
-        }
+    public class ArchivedBonusQuestionDetails
+    {
+        public int Id { get; set; }
+        public string? question { get; set; }
+        public string? img { get; set; }
+        public int Coins { get; set; } = 3;
 
-        public class correctanswers
-        {
-            public int Id { get; set; }
-            public string correctanswer { get; set; }
-        }
+        public ICollection<ArchivedCorrectAnswer> CorrectAnswers { get; set; }
+        public ICollection<ArchivedIncorrectAnswer> IncorrectAnswers { get; set; }
+    }
+
+    public class ArchivedCorrectAnswer
+    {
+        public int Id { get; set; }
+        public string correctanswer { get; set; }
+        public int BonusQuestionDetailsId { get; set; }
+        public ArchivedBonusQuestionDetails BonusQuestion { get; set; }
     }
 }
