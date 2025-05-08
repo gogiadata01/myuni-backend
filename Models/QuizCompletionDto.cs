831195b78ada7b2277f81e9b4a93d54a2f24b9a5
lukasordia123 <lksordia@gmail.com>
2025-05-08 22:34:16 +0400

    Update QuizCompletionDto.cs

--- Models/QuizCompletionDto.cs
+++ Models/QuizCompletionDto.cs
@@ -2,7 +2,7 @@ namespace MyUni.Models
 {
     public class QuizCompletionDto
 {
-    public string UserId { get; set; }
+    public int UserId { get; set; }
     public string CompletedDate { get; set; }
 }
 
