007a31965f6bcf9d33fe6d4eb7d9b0e6cc44a846
lukasordia123 <lksordia@gmail.com>
2025-05-29 14:01:44 +0400

    Update EventCard.cs

--- Models/Entities/EventCard.cs
+++ Models/Entities/EventCard.cs
@@ -7,7 +7,7 @@
         public string Title { get; set; }
         public string Text { get; set; }
         public string Time { get; set; }
-        public string Description  { get; set; }
+        // public string Description  { get; set; }
         public bool isFeatured { get; set; }
         public string Link { get; set; }
         public int Numbering { get; set; }
