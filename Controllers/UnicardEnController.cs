d7faba29786818c54ada5cfbda15b89feeb0be3a
lukasordia123 <lksordia@gmail.com>
2025-06-15 02:25:47 +0400

    Update UnicardEnController.cs

--- Controllers/UnicardEnController.cs
+++ Controllers/UnicardEnController.cs
@@ -35,6 +35,95 @@ namespace MyUni.Controllers
 
             return Ok(AllUniCard);
         }
+
+[HttpPost]
+public IActionResult AddUniCardEn([FromBody] UnicardEnDto addUniCardDto)
+{
+    if (!ModelState.IsValid)
+    {
+        return BadRequest(ModelState);
+    }
+
+    Console.WriteLine(JsonConvert.SerializeObject(addUniCardDto, Formatting.Indented));
+
+    var uniCardEntity = new UnicardEn
+    {
+        Url_en = addUniCardDto.url_en,
+        Title_en = addUniCardDto.title_en,
+        MainText_en = addUniCardDto.mainText_en,
+        History_en = addUniCardDto.history_en,
+        ForPupil_en = addUniCardDto.forPupil_en,
+        ScholarshipAndFunding_en = addUniCardDto.scholarshipAndFunding_en,
+        ExchangePrograms_en = addUniCardDto.exchangePrograms_en,
+        Labs_en = addUniCardDto.labs_en,
+        StudentsLife_en = addUniCardDto.studentsLife_en,
+        PaymentMethods_en = addUniCardDto.paymentMethods_en,
+        Events_en = addUniCardDto.events_en?.Select(e => new UnicardEn.Event_en
+        {
+            Url_en = e.url_en,
+            Title_en = e.title_en,
+            Text_en = e.text_en,
+            Time_en = e.time_en,
+            Link_en = e.link_en
+        }).ToList(),
+        Sections_en = addUniCardDto.sections_en?.Select(s => new UnicardEn.Section_en
+        {
+            Title_en = s.title_en,
+            ProgramNames_en = s.programNames_en?.Select(p => new UnicardEn.Programname_en
+            {
+                ProgramName_en = p.programName_en,
+                Jobs_en = p.jobs_en,
+                SwavlebisEna_en = p.swavlebisEna_en,
+                Kvalifikacia_en = p.kvalifikacia_en,
+                Dafinanseba_en = p.dafinanseba_en,
+                KreditebisRaodenoba_en = p.kreditebisRaodenoba_en,
+                AdgilebisRaodenoba_en = p.adgilebisRaodenoba_en,
+                Fasi_en = p.fasi_en,
+                Kodi_en = p.kodi_en,
+                ProgramisAgwera_en = p.programisAgwera_en
+            }).ToList()
+        }).ToList(),
+        Sections2_en = addUniCardDto.sections2_en?.Select(s2 => new UnicardEn.Section2_en
+        {
+            Title_en = s2.title_en,
+            SavaldebuloSagnebi_en = s2.savaldebuloSagnebi_en?.Select(ss => new UnicardEn.SavaldebuloSagnebi_en
+            {
+                SagnisSaxeli_en = ss.sagnisSaxeli_en,
+                Koeficienti_en = ss.koeficienti_en,
+                MinimaluriZgvari_en = ss.minimaluriZgvari_en,
+                Prioriteti_en = ss.prioriteti_en,
+                AdgilebisRaodenoba_en = ss.adgilebisRaodenoba_en
+            }).ToList()
+        }).ToList(),
+        ArchevitiSavaldebuloSaganebi_en = addUniCardDto.archevitiSavaldebuloSaganebi_en?.Select(a => new UnicardEn.ArchevitiSavaldebuloSagani_en
+        {
+            Title_en = a.title_en,
+            ArchevitiSavaldebuloSagnebi_en = a.archevitiSavaldebuloSagnebi_en?.Select(asb => new UnicardEn.ArchevitiSavaldebuloSagnebi_en
+            {
+                SagnisSaxeli_en = asb.sagnisSaxeli_en,
+                Koeficienti_en = asb.koeficienti_en,
+                MinimaluriZgvari_en = asb.minimaluriZgvari_en,
+                Prioriteti_en = asb.prioriteti_en,
+                AdgilebisRaodenoba_en = asb.adgilebisRaodenoba_en
+            }).ToList()
+        }).ToList()
+    };
+
+    dbContext.MyUniCardEn.Add(uniCardEntity);
+    dbContext.SaveChanges();
+
+    return Ok(uniCardEntity);
+}
+
+    }
+    
+    
+}
+
+
+
+
+
         //   [HttpPost]
         // public IActionResult AddUniCard([FromBody] UnicardEnDto addUniCardDto)
         // {
@@ -113,13 +202,3 @@ namespace MyUni.Controllers
 
         //     return Ok(UniCardEntity);
         // }
-
-
-    }
-    
-    
-}
-
-
-
-
