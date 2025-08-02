9a812f9a6b887ee0d509ef3932cd952d2f5b5f94
lukasordia123 <lksordia@gmail.com>
2025-08-03 00:54:38 +0400

    Update UserController.cs

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -37,6 +37,7 @@ public class UserController : ControllerBase
                 user.Img,
                 user.Coin,
                 user.Type,
+                user.Password,
                 user.RemainingTime
             })
             .ToList();
